3d2d17fbf71b12c34026eb26fe0d433d73a6d3f7
HUMTOKAMGA <andrew.humto123@gmail.com>
2020-01-05 03:58:17 +0100

    finish navbar

--- reservation/src/App.js
+++ reservation/src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import './App.css';
 
+import Home from './pages/Home'
+import Rooms from './pages/Rooms'
+import SingleRooms from './pages/SingleRooms'
+import Error from './pages/Error'
+import Navbar from './components/Navbar'
+import {Route, Switch} from 'react-router-dom'
+import {Fabeer} from 'react-icons/fa'
+
 function App() {
   return (
-    <>Hello world </>
+    <>
+    <Navbar />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/rooms" component={Rooms} />
+      <Route exact path="/rooms/:slug" component={SingleRooms} />
+      <Route component={Error}/>
+    </Switch>      
+    </>
   );
 }
 
