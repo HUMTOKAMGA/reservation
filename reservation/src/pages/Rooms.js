e4754b9d1744c50b29a57493bf2da47de571626c
HUMTOKAMGA <andrew.humto123@gmail.com>
2020-01-05 04:24:17 +0100

    Gestion des root

--- reservation/src/pages/Rooms.js
+++ reservation/src/pages/Rooms.js
@@ -1,10 +1,8 @@
 import React from 'react';
-
+import Hero from "../components/Hero"
 const Rooms = () => {
     return (
-        <div>
-            Hello from Rooms pages
-        </div>
+        <Hero hero="roomsHero"/>
     );
 }
 
