bb48b24872bd294f54fcd2873f4cfdca7750b341
HUMTOKAMGA <andrew.humto123@gmail.com>
2020-01-06 03:39:43 +0100

    Revert "add filter"
    This reverts commit 52fd5459621578c19f3795f59f262d923ce85e4c.

--- reservation/src/pages/Rooms.js
+++ reservation/src/pages/Rooms.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Hero from "../components/Hero"
 import Banner from "../components/Banner"
 import {Link} from "react-router-dom"
-import RoomsContainer from '../components/RoomsContainer'
-
 const Rooms = () => {
     return (
         <Hero hero="roomsHero"> 
